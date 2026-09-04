d8e442fdd5fa27dd6aac59889c6e3f56ea32096e
Aleksandar Milovanovic <aleksandar1034@yahoo.com>
2026-09-04 16:59:00 +0200

    init BankingService and SimpleBankingService

---
+++ src/main/java/com/example/banking/service/BankingService.java
@@ -0,0 +1,3 @@
+package com.example.banking.service;
+
+public interface BankingService {}
---
+++ src/main/java/com/example/banking/service/SimpleBankingService.java
@@ -0,0 +1,3 @@
+package com.example.banking.service;
+
+public final class SimpleBankingService implements BankingService {}
