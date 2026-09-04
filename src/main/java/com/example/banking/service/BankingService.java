fb203adcc8e92bc8d1acf45b50a97120fa06e42d
Aleksandar Milovanovic <aleksandar1034@yahoo.com>
2026-09-04 17:52:03 +0200

    Add functions to BankingService interface

--- src/main/java/com/example/banking/service/BankingService.java
+++ src/main/java/com/example/banking/service/BankingService.java
@@ -1,3 +1,16 @@
 package com.example.banking.service;
 
-public interface BankingService {}
+import com.example.banking.domain.AccountId;
+import com.example.banking.domain.Money;
+
+public interface BankingService {
+    AccountId createAccount(Money initialDeposit);
+
+    Money deposit(AccountId accountId, Money amount);
+
+    Money withdraw(AccountId accountId, Money amount);
+
+    TransferResult transfer(TransferRequest request);
+
+    Money getBalance(AccountId accountId);
+}
--- src/main/java/com/example/banking/service/SimpleBankingService.java
+++ src/main/java/com/example/banking/service/SimpleBankingService.java
@@ -1,3 +1,31 @@
 package com.example.banking.service;
 
-public final class SimpleBankingService implements BankingService {}
+import com.example.banking.domain.AccountId;
+import com.example.banking.domain.Money;
+
+public final class SimpleBankingService implements BankingService {
+    @Override
+    public AccountId createAccount(Money initialDeposit) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public Money deposit(AccountId accountId, Money amount) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public Money withdraw(AccountId accountId, Money amount) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public TransferResult transfer(TransferRequest request) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public Money getBalance(AccountId accountId) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
